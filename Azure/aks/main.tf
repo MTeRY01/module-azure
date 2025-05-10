f973679be851833482fad2dab415bfa9cd651eef
MTeRY01 <mterycraft2011@gmail.com>
2025-05-10 23:40:17 +0300

    added helm into aks module

--- Azure/aks/main.tf
+++ Azure/aks/main.tf
@@ -19,3 +19,40 @@ resource "azurerm_kubernetes_cluster" "aks" {
  
 }
 
+# Helm provider (uses kubectl from AKS)
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+    client_key            = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+# Install NGINX Ingress with Helm
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "nginx"
+  }
+  lifecycle {
+    ignore_changes = [
+      repository,  # Ignore changes to the Helm repo URL
+      chart,       # Ignore changes to the chart name
+      version,     # Ignore version updates
+      set,         # Ignore changes to `set` blocks
+      values       # Ignore changes to `values` files
+    ]
+  }
+} 
+
